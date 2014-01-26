261ca270c16a66216cc2b58f3dd8ae8cb39b137f
kingstone17 <hxljustdoit@gmail.com>
2014-01-26 11:08:54 +0800

    Add user microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,28 @@
+class MicropostsController < ApplicationController
+  before_filter :signed_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(params[:microposts])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+
+  def index
+  end
+
+  private
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+end
